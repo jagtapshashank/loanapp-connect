12c023e5c8ab73a2d0e645e3ae5b05cf58715595
jagtapshashank <shashanksantoshjagtap728@gmail.com>
2023-05-19 13:16:55 +0530

    second

--- app.py
+++ app.py
@@ -21,15 +21,11 @@ def predict():
     per_income = request.form.get('loan_percent_income')
     defaulter = request.form.get('cb_person_default_on_file')
 
-    #result =  {'person_income':income}
-
     input_query = np.array([[income,ownership,emp,l_grade,l_amount,int_rate,per_income,defaulter]], dtype=object)
 
     result = model.predict(input_query)[0]
 
     return jsonify({'Defaulter':str(result)})
-
-    #return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
